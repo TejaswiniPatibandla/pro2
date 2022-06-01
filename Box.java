2fcaa2e7ddc6e5951ae1d21685ae36c3b2db2928
TejaswiniPatibandla <106171965+TejaswiniPatibandla@users.noreply.github.com>
2022-06-01 07:41:38 +0530

    extended variable

--- Box.java
+++ Box.java
@@ -10,5 +10,7 @@ class Box {
   public static void main(String args[]){
     System.out.println("this is java program");
     A ab=new A;
+    A.show();
+    System.out.println("value of class A variable is"+a);
   }
 }
