0f73c2b882ba5efe9fb142fec023f0149cb2021e
TejaswiniPatibandla <106171965+TejaswiniPatibandla@users.noreply.github.com>
2022-06-01 07:39:38 +0530

    extended

--- Box.java
+++ Box.java
@@ -1,6 +1,14 @@
-
+class A extends Box
+{
+int a;
+void show()
+{
+System.out.println("child class");
+}
+}
 class Box {
   public static void main(String args[]){
     System.out.println("this is java program");
+    A ab=new A;
   }
 }
